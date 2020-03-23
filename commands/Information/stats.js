d1cb46ccd802d0f8d089c4a360d128cbc08cac26
Emy <me@emy.moe>
2020-03-23 23:19:05 +0100

    ✨ add stats command

--- commands/Information/stats.js
+++ commands/Information/stats.js
@@ -1,37 +1,43 @@
-const { Command, version: klasaVersion, Duration } = require('klasa');
-const { version: discordVersion } = require('discord.js');
+const { Command, Duration } = require('klasa');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			enabled: false,
+			enabled: true,
 			guarded: true,
 			description: language => language.get('COMMAND_STATS_DESCRIPTION')
 		});
 	}
 
-	async run(message) {
-		let [users, guilds, channels, memory] = [0, 0, 0, 0];
+	async run(msg) {
+		if (!await msg.hasAtLeastPermissionLevel(5)) {
+      if (!msg.guild.settings.channels.botspam) return;
+      if(msg.channel.id != msg.guild.settings.channels.botspam) {
+        return msg.send(`Command only allowed in <#${msg.guild.settings.channels.botspam}>`);
+      }
+    }
+		let [users, memory] = [0, 0];
 
 		if (this.client.shard) {
-			const results = await this.client.shard.broadcastEval(`[this.users.cache.size, this.guilds.cache.size, this.channels.cache.size, (process.memoryUsage().heapUsed / 1024 / 1024)]`);
+			const results = await this.client.shard.broadcastEval(`[this.users.cache.size, (process.memoryUsage().heapUsed / 1024 / 1024)]`);
 			for (const result of results) {
 				users += result[0];
-				guilds += result[1];
-				channels += result[2];
 				memory += result[3];
 			}
 		}
 
-		return message.sendCode('asciidoc', message.language.get('COMMAND_STATS',
-			(memory || process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2),
-			Duration.toNow(Date.now() - (process.uptime() * 1000)),
-			(users || this.client.users.cache.size).toLocaleString(),
-			(guilds || this.client.guilds.cache.size).toLocaleString(),
-			(channels || this.client.channels.cache.size).toLocaleString(),
-			klasaVersion, discordVersion, process.version, message
-		));
+		const embed = new MessageEmbed()
+			.setTitle('Statistics')
+			.setThumbnail(this.client.user.avatarURL({ format: 'jpg' }))
+			.setColor('GREEN')
+			.addField('Users', users || this.client.users.cache.size, true)
+			.addField('Memory', `${(memory || process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, true)
+			.addField('Uptime', Duration.toNow(Date.now() - (process.uptime() * 1000)), true)
+			.setTimestamp()
+		msg.send(embed);
+		msg.delete();
 	}
 
 };
