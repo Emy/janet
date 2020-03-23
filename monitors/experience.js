26faf25a99fd669a4442d01481587b97f46540ea
1Conan <me@1conan.com>
2020-03-23 09:04:50 +0800

    Add roles on level up

--- monitors/experience.js
+++ monitors/experience.js
@@ -14,12 +14,25 @@ module.exports = class extends Monitor {
   }
 
   async run(message) {
+    if (!message.guild) return;
     if (message.author.settings.xpFrozen) return;
     let gainedXP = Math.floor(Math.random() * 10 + 1)
     //if (message.member.lastMessage.content === message.content) gainedXP = gainedXP * -1;
     const currentXP = message.author.settings.xp;
     await message.author.settings.update('xp', currentXP + gainedXP);
-    await message.author.settings.update('level', this.getLevel(currentXP + gainedXP))
+    await message.author.settings.update('level', this.getLevel(currentXP + gainedXP));
+
+    if (message.author.settings.level >= 15 && message.guild.settings.roles.memberplus) {
+      await message.member.roles.add(message.guild.settings.roles.memberplus);
+    }
+
+    if (message.author.settings.level >= 30 && message.guild.settings.roles.memberpro) {
+      await message.member.roles.add(message.guild.settings.roles.memberpro);
+    }
+
+    if (message.author.settings.level >= 50 && message.guild.settings.roles.memberedition) {
+      await message.member.roles.add(message.guild.settings.roles.memberedition)
+    }
   }
 
   async init() {}
