87743b514fb552d7189f257b8351f53d7c2af6d3
Emy <me@emy.moe>
2020-03-28 00:37:19 +0100

    ♻️  cleaning

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,9 +1,11 @@
 import { GuildMember, MessageEmbed, TextChannel } from 'discord.js';
 import ASCIIFolder from 'fold-to-ascii';
-import { Event, EventStore, KlasaClient } from 'klasa';
+import { Event, EventStore } from 'klasa';
+import JanetClient from '../lib/client';
 
 export default class extends Event {
-    constructor(client: KlasaClient, store: EventStore, file: string[], dir: string) {
+    client: JanetClient;
+    constructor(client: JanetClient, store: EventStore, file: string[], dir: string) {
         super(client, store, file, dir, {
             enabled: true,
             once: false,
@@ -12,55 +14,58 @@ export default class extends Event {
 
     async run(oldMember: GuildMember, newMember: GuildMember) {
         if (!(oldMember || newMember)) return;
-        if (oldMember.nickname != newMember.nickname) {
-            const channelID = oldMember.guild.settings.get('channels.private');
-            if (!channelID) return;
+        if (oldMember.nickname != newMember.nickname) return this.nickNameChange(oldMember, newMember);
+        if (newMember.roles.cache.size != oldMember.roles.cache.size) return this.roleChange(oldMember, newMember);
+    }
 
-            const nick = ASCIIFolder.foldMaintaining(newMember.displayName).toLowerCase();
+    nickNameChange(oldMember: GuildMember, newMember: GuildMember) {
+        const channelID = oldMember.guild.settings.get('channels.private');
+        if (!channelID) return;
 
-            if (oldMember.guild.settings.get('filter.enableWordFiltering')) {
-                for (const filteredWord of oldMember.guild.settings.get('filter.words')) {
-                    if (!nick.includes(filteredWord.word.toLowerCase())) continue;
-                    newMember.setNickname('change name pls', 'filtered word');
-                }
+        const nick = ASCIIFolder.foldMaintaining(newMember.displayName).toLowerCase();
+
+        if (oldMember.guild.settings.get('filter.enableWordFiltering')) {
+            for (const filteredWord of oldMember.guild.settings.get('filter.words')) {
+                if (!nick.includes(filteredWord.word.toLowerCase())) continue;
+                newMember.setNickname('change name pls', 'filtered word');
             }
-
-            const embed = new MessageEmbed()
-                .setTitle('Member Renamed')
-                .setThumbnail(oldMember.user.avatarURL({ format: 'jpg' }))
-                .setColor('ORANGE')
-                .addField('Member', `${oldMember.user.tag} (<@${oldMember.id}>)`)
-                .addField('Old Nickname', oldMember.nickname ? oldMember.nickname : 'No Nickname', true)
-                .addField('New Nickname', newMember.nickname ? newMember.nickname : 'No Nickname', true)
-                .setFooter(oldMember.user.id)
-                .setTimestamp();
-
-            const channel = this.client.channels.cache.get(channelID) as TextChannel;
-            channel.send(embed);
         }
 
-        if (newMember.roles.cache.size != oldMember.roles.cache.size) {
-            const channelID = oldMember.guild.settings.get('channels.private');
-            if (!channelID) return;
-            const newRole = newMember.roles.cache.difference(oldMember.roles.cache);
+        const embed = new MessageEmbed()
+            .setTitle('Member Renamed')
+            .setThumbnail(oldMember.user.avatarURL({ format: 'jpg' }))
+            .setColor('ORANGE')
+            .addField('Member', `${oldMember.user.tag} (<@${oldMember.id}>)`)
+            .addField('Old Nickname', oldMember.nickname ? oldMember.nickname : 'No Nickname', true)
+            .addField('New Nickname', newMember.nickname ? newMember.nickname : 'No Nickname', true)
+            .setFooter(oldMember.user.id)
+            .setTimestamp();
 
-            newRole.delete(newMember.guild.settings.get('roles.member'));
-            if (newRole.size < 1) return;
+        const channel = this.client.channels.cache.get(channelID) as TextChannel;
+        channel.send(embed);
+    }
 
-            const embedTitle =
-                newMember.roles.cache.size > oldMember.roles.cache.size ? 'Member Role Added' : 'Member Role Removed';
+    roleChange(oldMember: GuildMember, newMember: GuildMember) {
+        const channelID = oldMember.guild.settings.get('channels.private');
+        if (!channelID) return;
+        const newRole = newMember.roles.cache.difference(oldMember.roles.cache);
 
-            const embed = new MessageEmbed()
-                .setTitle(embedTitle)
-                .setThumbnail(oldMember.user.avatarURL({ format: 'jpg' }))
-                .setColor('BLUE')
-                .addField('Member', `${oldMember.user.tag} (<@${oldMember.id}>)`)
-                .addField('Role', `${newRole.map((role) => role.name)}`, true)
-                .setFooter(oldMember.user.id)
-                .setTimestamp();
+        newRole.delete(newMember.guild.settings.get('roles.member'));
+        if (newRole.size < 1) return;
 
-            const channel = this.client.channels.cache.get(channelID) as TextChannel;
-            channel.send(embed);
-        }
+        const embedTitle =
+            newMember.roles.cache.size > oldMember.roles.cache.size ? 'Member Role Added' : 'Member Role Removed';
+
+        const embed = new MessageEmbed()
+            .setTitle(embedTitle)
+            .setThumbnail(oldMember.user.avatarURL({ format: 'jpg' }))
+            .setColor('BLUE')
+            .addField('Member', `${oldMember.user.tag} (<@${oldMember.id}>)`)
+            .addField('Role', `${newRole.map((role) => role.name)}`, true)
+            .setFooter(oldMember.user.id)
+            .setTimestamp();
+
+        const channel = this.client.channels.cache.get(channelID) as TextChannel;
+        channel.send(embed);
     }
 }
