4fbef843432ec23e5a6972c5152ce38733b2c313
Emy <me@emy.moe>
2020-03-28 00:38:30 +0100

    🚧  wip play command. (playlists don't work atm)

--- src/commands/Voice/play.ts
+++ src/commands/Voice/play.ts
@@ -1,52 +1,46 @@
-import { Command, CommandStore, KlasaClient, KlasaMessage } from 'klasa';
-// const fetch = require('node-fetch');
+import { Command, CommandStore, KlasaMessage } from 'klasa';
+import { LoadTrackResponse } from 'shoukaku';
+import JanetClient from '../../lib/client';
 
 export default class extends Command {
-    constructor(client: KlasaClient, store: CommandStore, file: string[], dir: string) {
+    client: JanetClient;
+    constructor(client: JanetClient, store: CommandStore, file: string[], dir: string) {
         super(client, store, file, dir, {
-            enabled: false,
+            enabled: true,
             runIn: ['text'],
             requiredPermissions: ['EMBED_LINKS'],
             aliases: ['p'],
             cooldown: 5,
             description: (lang) => lang.get('PLAY_DESCRIPTION'),
-            usage: '<track:string>',
+            usage: '<track:...string>',
+            usageDelim: ' ',
         });
     }
 
-    async run(msg: KlasaMessage, [song]) {
-        // const lang = msg.language;
-        // if (!msg.member.voice.channel) return msg.sendError('NOT_IN_VC');
-        // const node = this.client.shoukaku.getNode();
-        // let tracks = await node.rest.resolve(song);
-        // if (!tracks) tracks = await node.rest.resolve(song, 'youtube');
-        // if (!tracks) {
-        //   return 'no tracks found...';
-        // } // no tracks found
-        // if (Array.isArray(tracks)) {
-        //   const dispatcher = await this.client.queue.handleTrack(node, tracks.shift(), msg);
-        //   tracks.forEach((track) => {
-        //     this.client.queue.handleTrack(node, track, msg);
-        //   });
-        //   // Added playlist
-        //   if (dispatcher) return await dispatcher.play();
-        //   // Now playing
-        // }
+    async run(msg: KlasaMessage, [song]: [string]) {
+        if (!msg.member.voice.channel) return msg.send('NOT_IN_VC');
+        const node = this.client.shoukaku.getNode();
+        const tracks = await node.rest.resolve(song, 'youtube');
+        if (!tracks) return msg.send('No tracks found.');
+        if (Array.isArray(tracks)) {
+            const dispatcher = await this.client.queue.handleTrack(node, tracks.shift(), msg);
+            tracks.forEach((track) => {
+                this.client.queue.handleTrack(node, track, msg);
+            });
+            // Added playlist
+            if (dispatcher) await dispatcher.play();
+            return null;
+            // Now playing
+        }
 
-        // if (tracks.track) {
-        //   const dispatcher = await this.client.queue.handleTrack(node, tracks, msg);
-        //   if (dispatcher) return await dispatcher.play();
-        // }
+        // Should be a LoadTrackResponse at this point.
+        const ltr = tracks as LoadTrackResponse;
 
-        // if (tracks.loadType === 'SEARCH_RESULT' && tracks.tracks) {
-        //   let index = 0;
-        //   if (tracks.tracks.length > 1) {
-        //     // prompt
-        //   }
-        //   const dispatcher = await this.client.queue.handleTrack(node, tracks.tracks[index], msg);
-        //   if (dispatcher) return await dispatcher.play();
-        // }
+        if (Array.isArray(ltr.tracks)) {
+            const dispatcher = await this.client.queue.handleTrack(node, ltr.tracks[0], msg);
+            if (dispatcher) await dispatcher.play();
+        }
 
-        return null;
+        return msg.send('hmm');
     }
 }
