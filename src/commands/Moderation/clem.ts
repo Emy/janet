3c84750954e04a7009dc825af2616a31282dca0f
Revulate <36025481+Revulate@users.noreply.github.com>
2020-03-31 11:01:08 -0700

    Updated clem to reset xp and level to 0

--- src/commands/Moderation/clem.ts
+++ src/commands/Moderation/clem.ts
@@ -29,6 +29,8 @@ export default class extends Command {
     async run(msg: KlasaMessage, [member, reason]: [GuildMember, string]) {
         await member.user.settings.update('clem', true);
         await member.user.settings.update('xpFrozen', true);
+        await member.user.settings.update('xp', 0);
+        await member.user.settings.update('level', 0);
         const warnPointDiff = 599 - member.user.settings.get('warnPoints');
         await member.user.settings.update('warnPoints', 599);
         const c = await this.buildCase(msg, reason, member.user, warnPointDiff);
